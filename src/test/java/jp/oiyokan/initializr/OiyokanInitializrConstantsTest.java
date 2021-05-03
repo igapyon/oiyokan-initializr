95569be22479514c4a33c585f720f21695b5b4a3
Toshiki Iga <igapyon@gmail.com>
2021-05-04 07:32:28 +0900

    Initial : start to develop (#1)
    Initial : start to develop

---
+++ src/test/java/jp/oiyokan/initializr/OiyokanInitializrConstantsTest.java
@@ -0,0 +1,14 @@
+package jp.oiyokan.initializr;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class OiyokanInitializrConstantsTest {
+
+    @Test
+    void test() {
+        if (false)
+            fail("Not yet implemented");
+    }
+}
