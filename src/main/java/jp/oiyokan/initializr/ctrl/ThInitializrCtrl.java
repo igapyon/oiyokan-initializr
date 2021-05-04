395039927dca41fcd4a272862cd6b3a750abdb32
Toshiki Iga <igapyon@gmail.com>
2021-05-04 19:40:59 +0900

    savee

--- src/main/java/jp/oiyokan/initializr/ctrl/ThInitializrCtrl.java
+++ src/main/java/jp/oiyokan/initializr/ctrl/ThInitializrCtrl.java
@@ -34,8 +34,10 @@ public class ThInitializrCtrl {
     private static final Log log = LogFactory.getLog(ThInitializrCtrl.class);
 
     @RequestMapping(value = { "/initializr" }, method = { RequestMethod.GET })
-    public String index(Model model, OiyoSettingsDatabase database, BindingResult result) throws IOException {
+    public String index(Model model, OiyoSettingsDatabase database, ThInitializrBean initializerBean,
+            BindingResult result) throws IOException {
         model.addAttribute("databaseBean", database);
+        model.addAttribute("initializerBean", initializerBean);
         model.addAttribute("msgSuccess", "");
         model.addAttribute("msgError", "");
 
@@ -61,8 +63,10 @@ public class ThInitializrCtrl {
     }
 
     @RequestMapping(value = { "/initializr" }, params = { "connTest" }, method = { RequestMethod.POST })
-    public String connTest(Model model, OiyoSettingsDatabase database, BindingResult result) throws IOException {
+    public String connTest(Model model, OiyoSettingsDatabase database, ThInitializrBean initializerBean,
+            BindingResult result) throws IOException {
         model.addAttribute("databaseBean", database);
+        model.addAttribute("initializerBean", initializerBean);
         model.addAttribute("msgSuccess", "");
         model.addAttribute("msgError", "");
 
@@ -122,8 +126,10 @@ public class ThInitializrCtrl {
     }
 
     @RequestMapping(value = { "/initializr" }, params = "download", method = { RequestMethod.POST })
-    public String entry(Model model, OiyoSettingsDatabase database, HttpServletResponse response) throws IOException {
+    public String download(Model model, OiyoSettingsDatabase database, ThInitializrBean initializerBean,
+            HttpServletResponse response) throws IOException {
         model.addAttribute("databaseBean", database);
+        model.addAttribute("initializerBean", initializerBean);
         model.addAttribute("msgSuccess", "");
         model.addAttribute("msgError", "");
 
@@ -145,9 +151,6 @@ public class ThInitializrCtrl {
 
         oiyoSettings.getDatabase().add(database);
 
-        boolean convertCamel = false; // EntitySetなどの名称を Camel case にするかどうか。通常は false で良い
-        boolean isSfdcMode = true; // Support Salesforce or not.
-
         //////////////////////////////////////////////////////////
         // Process settings
 
@@ -163,7 +166,7 @@ public class ThInitializrCtrl {
             database.setDescription(OiyokanInitializrMessages.IYI2202);
         }
 
-        OiyokanInitializrUtil.tuneSettings(oiyoInfo, oiyoSettings, convertCamel, isSfdcMode);
+        OiyokanInitializrUtil.tuneSettings(oiyoInfo, oiyoSettings, initializerBean.isConvertCamel(), initializerBean.isFilterTreatNullAsBlank);
 
         //////////////////////////////////////////////////////////
         // Write settings info into oiyokan-settings.json
@@ -200,5 +203,126 @@ public class ThInitializrCtrl {
         // [IYI1002] Oiyokan Initializr End.
         log.info(OiyokanInitializrMessages.IYI1002);
         return null;
+    }
+
+    @RequestMapping(value = { "/initializr" }, params = { "preH2" }, method = { RequestMethod.POST })
+    public String preH2(Model model, OiyoSettingsDatabase database, ThInitializrBean initializerBean,
+            BindingResult result) throws IOException {
+        model.addAttribute("databaseBean", database);
+        model.addAttribute("initializerBean", initializerBean);
+        model.addAttribute("msgSuccess", "");
+        model.addAttribute("msgError", "");
+
+        if (database.getName() == null) {
+            database.setName("mydbsetting1");
+        }
+        database.setType("h2"); // h2, PostgreSQL, MySQL, SQLSV2008, ORCL18
+        if (database.getDescription() == null) {
+            database.setDescription("Tutorial db sample.");
+        }
+        database.setJdbcDriver("org.h2.Driver"); // JDBC Driver class name.
+        database.setJdbcUrl("jdbc:h2:file:./oiyokanTest;DB_CLOSE_DELAY=-1;DATABASE_TO_UPPER=FALSE;MODE=MSSQLServer");
+        database.setJdbcUser("sa"); // JDBC User.
+        database.setJdbcPassPlain(""); // JDBC Password.
+
+        model.addAttribute("msgSuccess", "Typical h2 preset is loaded.");
+        return "oiyokan/initializr";
+    }
+
+    @RequestMapping(value = { "/initializr" }, params = { "prePostgreSQL" }, method = { RequestMethod.POST })
+    public String prePostgreSQL(Model model, OiyoSettingsDatabase database, ThInitializrBean initializerBean,
+            BindingResult result) throws IOException {
+        model.addAttribute("databaseBean", database);
+        model.addAttribute("initializerBean", initializerBean);
+        model.addAttribute("msgSuccess", "");
+        model.addAttribute("msgError", "");
+
+        if (database.getName() == null) {
+            database.setName("mydbsetting1");
+        }
+        database.setType("PostgreSQL"); // h2, PostgreSQL, MySQL, SQLSV2008, ORCL18
+        if (database.getDescription() == null) {
+            database.setDescription("Tutorial db sample.");
+        }
+        database.setJdbcDriver("org.postgresql.Driver"); // JDBC Driver class name.
+        database.setJdbcUrl("jdbc:postgresql://localhost:5432/dvdrental");
+        database.setJdbcUser(""); // JDBC User.
+        database.setJdbcPassPlain(""); // JDBC Password.
+
+        model.addAttribute("msgSuccess", "Typical PostgreSQL preset is loaded.");
+        return "oiyokan/initializr";
+    }
+
+    @RequestMapping(value = { "/initializr" }, params = { "preMySQL" }, method = { RequestMethod.POST })
+    public String preMySQL(Model model, OiyoSettingsDatabase database, ThInitializrBean initializerBean,
+            BindingResult result) throws IOException {
+        model.addAttribute("databaseBean", database);
+        model.addAttribute("initializerBean", initializerBean);
+        model.addAttribute("msgSuccess", "");
+        model.addAttribute("msgError", "");
+
+        if (database.getName() == null) {
+            database.setName("mydbsetting1");
+        }
+        database.setType("MySQL"); // h2, PostgreSQL, MySQL, SQLSV2008, ORCL18
+        if (database.getDescription() == null) {
+            database.setDescription("Tutorial db sample.");
+        }
+        database.setJdbcDriver("com.mysql.jdbc.Driver"); // JDBC Driver class name.
+        database.setJdbcUrl(
+                "jdbc:mysql://localhost/mysql?useUnicode=true&characterEncoding=UTF-8&characterSetResults=UTF-8&useCursorFetch=true&defaultFetchSize=128&useServerPrepStmts=true&emulateUnsupportedPstmts=false");
+        database.setJdbcUser("root"); // JDBC User.
+        database.setJdbcPassPlain("passwd123"); // JDBC Password.
+
+        model.addAttribute("msgSuccess", "Typical MySQL preset is loaded.");
+        return "oiyokan/initializr";
+    }
+
+    @RequestMapping(value = { "/initializr" }, params = { "preSQLSV2008" }, method = { RequestMethod.POST })
+    public String preSQLSV2008(Model model, OiyoSettingsDatabase database, ThInitializrBean initializerBean,
+            BindingResult result) throws IOException {
+        model.addAttribute("databaseBean", database);
+        model.addAttribute("initializerBean", initializerBean);
+        model.addAttribute("msgSuccess", "");
+        model.addAttribute("msgError", "");
+
+        if (database.getName() == null) {
+            database.setName("mydbsetting1");
+        }
+        database.setType("SQLSV2008"); // h2, PostgreSQL, MySQL, SQLSV2008, ORCL18
+        if (database.getDescription() == null) {
+            database.setDescription("Tutorial db sample.");
+        }
+        database.setJdbcDriver("com.microsoft.sqlserver.jdbc.SQLServerDriver"); // JDBC Driver class name.
+        database.setJdbcUrl("jdbc:sqlserver://localhost\\SQLExpress");
+        database.setJdbcUser("sa"); // JDBC User.
+        database.setJdbcPassPlain("passwd123"); // JDBC Password.
+
+        model.addAttribute("msgSuccess", "Typical SQLSV2008 preset is loaded.");
+        return "oiyokan/initializr";
+    }
+
+    @RequestMapping(value = { "/initializr" }, params = { "preORCL18" }, method = { RequestMethod.POST })
+    public String preORCL18(Model model, OiyoSettingsDatabase database, ThInitializrBean initializerBean,
+            BindingResult result) throws IOException {
+        model.addAttribute("databaseBean", database);
+        model.addAttribute("initializerBean", initializerBean);
+        model.addAttribute("msgSuccess", "");
+        model.addAttribute("msgError", "");
+
+        if (database.getName() == null) {
+            database.setName("mydbsetting1");
+        }
+        database.setType("ORCL18"); // h2, PostgreSQL, MySQL, SQLSV2008, ORCL18
+        if (database.getDescription() == null) {
+            database.setDescription("Tutorial db sample.");
+        }
+        database.setJdbcDriver("oracle.jdbc.driver.OracleDriver"); // JDBC Driver class name.
+        database.setJdbcUrl("jdbc:oracle:thin:@10.0.2.15:1521/xepdb1");
+        database.setJdbcUser("orauser"); // JDBC User.
+        database.setJdbcPassPlain("passwd123"); // JDBC Password.
+
+        model.addAttribute("msgSuccess", "Typical ORCL18 preset is loaded.");
+        return "oiyokan/initializr";
     }
 }
