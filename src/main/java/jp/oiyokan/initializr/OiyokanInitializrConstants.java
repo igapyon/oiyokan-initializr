61cc2ef7cdebd01cff48143b695ad0bd4f52aeda
Toshiki Iga <igapyon@gmail.com>
2021-05-09 19:23:22 +0900

    increment versino.

--- src/main/java/jp/oiyokan/initializr/OiyokanInitializrConstants.java
+++ src/main/java/jp/oiyokan/initializr/OiyokanInitializrConstants.java
@@ -19,5 +19,5 @@ package jp.oiyokan.initializr;
  * Oiyokan Initializr の定数.
  */
 public class OiyokanInitializrConstants {
-    public static final String VERSION = "0.2.20210505a";
+    public static final String VERSION = "0.3.20210509a";
 }
