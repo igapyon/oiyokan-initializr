47f525939fac8ce1a2e531a723cb07b542b29097
Toshiki Iga <igapyon@gmail.com>
2021-06-03 22:13:00 +0900

    update lib version.

--- src/main/java/jp/oiyokan/initializr/OiyokanInitializrConstants.java
+++ src/main/java/jp/oiyokan/initializr/OiyokanInitializrConstants.java
@@ -19,7 +19,7 @@ package jp.oiyokan.initializr;
  * Oiyokan Initializr の定数.
  */
 public class OiyokanInitializrConstants {
-    public static final String VERSION = "1.0.20210528a";
+    public static final String VERSION = "1.1.20210603a";
 
     /**
      * 通常は FALSE 運用.
